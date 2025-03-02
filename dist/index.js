f6937bd51e443f701fde1444656ddbdaf73c2c12
reversesweep <derekha19@gmail.com>
2025-03-01 23:52:04 -0800

    flask

--- dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-function greet(name) {
-    return `Hello, ${name}!`;
-}
-const myName = "World";
-console.log(greet(myName));
--- src/index.ts
+++
@@ -1,6 +0,0 @@
-function greet(name: string): string {
-    return `Hello, ${name}!`;
-  }
-  
-  const myName: string = "World";
-  console.log(greet(myName));
